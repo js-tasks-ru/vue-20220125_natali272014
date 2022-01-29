6b3c9b8e6cb03768d77527e606d9028657f3c2b4
Natali Khudyakova <natali272014@gmail.com>
2022-01-29 19:11:12 +0400

    CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,13 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent, createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const Component = defineComponent({
+  name: 'Component',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+});
+
+const app = createApp(Component);
+const vm = app.mount('#app');
