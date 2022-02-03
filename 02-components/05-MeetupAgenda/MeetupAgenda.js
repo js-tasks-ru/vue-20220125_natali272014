e7d846603dee5d72a8238e921a15f003c3aefbd1
Natali Khudyakova <natali272014@gmail.com>
2022-02-03 14:52:49 +0400

    MeetupAgenda

--- 02-components/05-MeetupAgenda/MeetupAgenda.js
+++ 02-components/05-MeetupAgenda/MeetupAgenda.js
@@ -1,13 +1,21 @@
 import { defineComponent } from './vendor/vue.esm-browser.js';
-// import MeetupAgendaItem from './MeetupAgendaItem.js';
+import MeetupAgendaItem from './MeetupAgendaItem.js';
 
 export default defineComponent({
   name: 'MeetupAgenda',
 
+  components: {
+    MeetupAgendaItem,
+  },
+
+  props: ['agenda'],
+
   template: `
     <ul class="agenda">
       <li class="agenda__item">
-        <!-- meetup agenda item -->
+        <template  v-for="agenda in agenda" :key="agenda.id" >
+          <meetup-agenda-item :agenda-item="agenda"></meetup-agenda-item>
+        </template>
       </li>
     </ul>`,
 });
