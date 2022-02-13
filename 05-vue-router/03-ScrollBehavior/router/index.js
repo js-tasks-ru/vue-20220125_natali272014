36e4c94f51b5c0294c155d693f8fa5a4a70663fc
Natali Khudyakova <natali272014@gmail.com>
2022-02-13 13:18:45 +0400

    ScrollBehavior

--- 05-vue-router/03-ScrollBehavior/router/index.js
+++ 05-vue-router/03-ScrollBehavior/router/index.js
@@ -2,7 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/03-ScrollBehavior'),
-
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else if (to.hash) {
+      return {
+        el: to.hash,
+      };
+    } else if ((to.meta.saveScrollPosition || from.meta.saveScrollPosition) && from.meta.saveScrollPosition === to.meta.saveScrollPosition) {
+      return false;
+    } else {
+      return { top: 0, left: 0 };
+    }
+  },
   routes: [
     {
       path: '/',
@@ -21,6 +33,7 @@ export const router = createRouter({
         showReturnToMeetups: true,
         saveScrollPosition: true,
       },
+
       props: true,
       redirect: (to) => ({ name: 'meetup.description', params: to.params }),
       component: () => import('../views/PageMeetup'),
