dd5976fd63629c77f33786c7ccc95c369bf3cf4e
Natali Khudyakova <natali272014@gmail.com>
2022-02-22 13:13:05 +0400

    MeetupForm

--- 07-forms/02-MeetupForm/components/UiInputDate.vue
+++ 07-forms/02-MeetupForm/components/UiInputDate.vue
@@ -1,5 +1,9 @@
 <template>
-  <ui-input :model-value="value" @input="handleInput" />
+  <ui-input ref="input" v-model="modelValueProxy" v-bind="$attrs" :type="type">
+    <template v-for="slotName in Object.keys($slots)" #[slotName]>
+      <slot :name="slotName" />
+    </template>
+  </ui-input>
 </template>
 
 <script>
@@ -7,27 +11,39 @@ import UiInput from './UiInput';
 
 export default {
   name: 'UiInputDate',
-
   components: { UiInput },
-
+  inheritAttrs: false,
   props: {
+    type: {
+      type: String,
+      default: 'date',
+    },
     modelValue: Number,
   },
 
   emits: ['update:modelValue'],
-
   computed: {
-    value() {
-      if (!this.modelValue) {
-        return '';
-      }
-      return new Date(this.modelValue).toISOString().substring(0, 10);
+    modelValueProxy: {
+      get() {
+        if (this.modelValue) return this.formatAsIsoDate(this.modelValue);
+        else return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', this.$refs.input.$refs.input.valueAsNumber);
+      },
     },
   },
-
   methods: {
-    handleInput($event) {
-      this.$emit('update:modelValue', $event.target.value === '' ? null : $event.target.valueAsNumber);
+    formatAsIsoDate(timestamp) {
+      if (this.type === 'date') return new Date(timestamp).toISOString().split('T')[0];
+      else if (this.type === 'time' && this.$attrs.step % 60 !== 0)
+        return new Date(timestamp).toISOString().split('T')[1].split('.')[0].substring(0, 5);
+      else if (this.type === 'time' && this.$attrs.step % 60 === 0)
+        return new Date(timestamp).toISOString().split('T')[1].split('.')[0].substring(0, 5);
+      else if (this.type === 'time' && !this.$attrs.step)
+        return new Date(timestamp).toISOString().split('T')[1].split('.')[0].substring(0, 5);
+      else if (this.type === 'datetime-local') return new Date(timestamp).toISOString().substring(0, 16);
+      else return new Date(timestamp);
     },
   },
 };
